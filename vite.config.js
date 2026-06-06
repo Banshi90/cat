73fa02d31905fdc986b4171f330cae7311d07979
Banshi90 <shnehts@mail.ru>
2026-06-06 13:14:12 +0500

    Add GitHub Actions deploy and fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // ← ВАЖНО! Для GitHub Pages
+  base: '/cat/',  // ← ВАЖНО! Для GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
